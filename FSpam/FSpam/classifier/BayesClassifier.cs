4c2e854491d9806ae3414158b3ada357b911fcc0
ivica.celig <ivica.celig@ecx.io>
2019-01-06 13:27:44 +0100

    Added classifier folder

---
+++ FSpam/FSpam/classifier/BayesClassifier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace FSpam.classifier
+{
+    class BayesClassifier
+    {
+        public void LoadTokens(System.Collections.Generic.List SetATokens, System.Collections.Generic.List SetBTokens)
+          {
+              foreach (T TokenA in SetATokens)
+              {
+ 119:                 SetA.Add(TokenA);
+ 120:             }
+ 121:             foreach (T TokenB in SetBTokens)
+ 122:             {
+ 123:                 SetB.Add(TokenB);
+ 124:             }
+ 125:             TotalA = 0;
+ 126:             TotalB = 0;
+ 127:             foreach (T Token in SetA)
+ 128:             {
+ 129:                 TotalA += SetA[Token];
+ 130:             }
+ 131:             foreach (T Token in SetB)
+ 132:             {
+ 133:                 TotalB += SetB[Token];
+ 134:             }
+ 135:             Total = TotalA + TotalB;
+ 136:             Probabilities = new Dictionary<T, double>();
+ 137:             foreach (T Token in SetA)
+ 138:             {
+ 139:                 Probabilities.Add(Token, CalculateProbabilityOfToken(Token));
+ 140:             }
+ 141:             foreach (T Token in SetB)
+ 142:             {
+ 143:                 if (!Probabilities.ContainsKey(Token))
+ 144:                 {
+ 145:                     Probabilities.Add(Token, CalculateProbabilityOfToken(Token));
+ 146:                 }
+ 147:             }
+ 148:         }
+    }
+}
---
+++ FSpam/FSpam/classifier/TokenLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSpam.classifier
+{
+    class TokenLoader
+    {
+        public void LoadTokens(List<String> SetATokens, System.Collections.Generic.List<String> SetBTokens)
+        {
+
+
+
+        }
+    }
+}
+}
