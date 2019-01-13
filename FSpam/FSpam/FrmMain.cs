08ccbd2ba3af3f9b655771d3d52423dd421c7a3d
ivica.celig <ivica.celig@ecx.io>
2019-01-13 18:48:42 +0100

    Updated gitignore

---
+++ FSpam/FSpam/FrmMain.cs
@@ -0,0 +1,49 @@
+﻿using FSpam.mail;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSpam
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            MailLoader mail = new MailLoader();
+            mail.readMail(txtUsername.Text,txtPassword.Text);
+
+            if(mail.zastavica != 0)
+            {
+                FrmMails formMails = new FrmMails(mail.titles, mail.summaries, mail.senders);
+                formMails.ShowDialog();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FSpam/FSpam/classifier/BayesClassifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace FSpam.classifier
+{
+    class BayesClassifier
+    {
+        TokenLoader tokenLoader = new TokenLoader();
+        public void classify()
+        {
+          
+
+            }        
+    }
+}
